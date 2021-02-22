9e45d41c32d46360b7621b39445effb6571d99a9
Simon Geilfus <simongeilfus@users.noreply.github.com>
2021-02-22 10:44:16 +0100

    GlslApp: Minor cleanup

--- tests/Glsl/src/GlslApp.cpp
+++ tests/Glsl/src/GlslApp.cpp
@@ -19,7 +19,6 @@ public:
 
 void GlslApp::setup()
 {
-    // Pipeline state object encompasses configuration of all GPU stages
     mPipelineState = gx::createGraphicsPipelineState( gx::GraphicsPipelineStateCreateInfo()
         .vertexShader( gx::createShader( gx::ShaderCreateInfo()
             .shaderType( gx::SHADER_TYPE_VERTEX )
@@ -45,7 +44,6 @@ void GlslApp::setup()
                 }
             )" )
         ) )
-        // Pixel shader simply outputs interpolated vertex color
         .pixelShader( gx::createShader( gx::ShaderCreateInfo()
             .shaderType( gx::SHADER_TYPE_PIXEL )
             .sourceLanguage( gx::SHADER_SOURCE_LANGUAGE_GLSL )
@@ -63,14 +61,9 @@ void GlslApp::setup()
 
 void GlslApp::draw()
 {
-    // Clear the back buffer
     gx::clear( ColorA( 0.350f, 0.350f, 0.350f, 1.0f ) );
-    // Set the pipeline state in the immediate context
     gx::setPipelineState( mPipelineState );
-    // Typically we should now call CommitShaderResources(), however shaders in this example don't
-    // use any resources.
-    // We will render 3 vertices
     gx::draw( gx::DrawAttribs().numVertices( 3 ) );
 }
 
-CINDER_APP( GlslApp, RendererGx/*( RendererGx::Options().deviceType( gx::RENDER_DEVICE_TYPE_D3D12 ) )*/ )
+CINDER_APP( GlslApp, RendererGx )
